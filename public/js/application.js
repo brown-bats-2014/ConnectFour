d06ae50104cd3176e4fd004ea807e201e71a505c
Supakorn Laohasongkram <>
2014-10-16 17:08:17 -0700

    controller&view

--- public/js/application.js
+++ public/js/application.js
@@ -1,26 +1,43 @@
-$(document).ready(function() {
   //EventListener
   for (var i = 0; i < 42; i++)
   $("tr").on( "click", function (e) {
     e.preventDefault();
-    // //counter for altenating players
+    //counter for altenating players
     var counter = 1
     var notDone = true
     var column = this
     var columnId = this.id
-    // // Alternating Player
-    // while (notDone) {
-    //   // Player1
-    //   if (counter % 2 == 0) {
-    //     User2.dropPiece();
-    //     counter++;
-    //   };
-    //   // Player2
-    //   else {
-    //     User1.dropPiece();
-    //     counter++;
-    //   };
-    debugger;
+
+
+    // User1
+    var User1 = function () {
+      this.dropPiece = function () {
+        debugger;
+      };
+    };
+    //
+
+    // User2
+    var User2 = function () {
+      this.dropPiece = function () {
+        debugger;
+      };
+    };
+    //
+    // Alternating Player
+      // while (notDone) {
+      //Player1
+      if (counter % 2 == 0) {
+        var user2 = new User2();
+        user2.dropPiece();
+        counter++;
+      }
+      // Player2
+      else {
+        var user1 = new User1();
+        user1.dropPiece();
+        counter++;
+      }
     // };
   });
 
@@ -31,20 +48,5 @@ $(document).ready(function() {
   };
   //
 
-  // User1
-  var User1 = function () {
-    this.dropPiece = function () {
 
-    };
-  };
-  //
 
-  // User2
-  var User2 = function () {
-    this.dropPiece = function () {
-
-    };
-  };
-  //
-
-});
